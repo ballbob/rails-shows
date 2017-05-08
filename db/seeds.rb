fec772f140d2d9336c30e16a07682bcc6020ba9d
Robert Ball <robert.ball.1989@gmail.com>
2017-05-08 15:35:08 +0100

    Added seeds and images.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Show.delete_all
+
+Show.create({
+  title: "PaulRu's Dragnet",
+  series: 9,
+  description: "The search for Luxembourg's next cop superstar",
+  image: "cop.jpeg",
+  programmeID: "98989898"
+})
+
+Show.create({
+  title: "Don't Tell the Groom",
+  series: 1,
+  description: "We've given the Bride 24 hours and $260,000 in unmarked bills to assassinate her man.",
+  image: "groom.jpeg",
+  programmeID: "98989898"
+})
